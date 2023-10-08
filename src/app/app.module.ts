78423496a5edb05a829edba93c47b1a03509fcbe
jaderaposa <jaduya678@gmail.com>
2023-10-08 21:58:05 +0800

    setting up and designing of header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Routes,RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { DataComponent } from './data/data.component';
+import { DatatableComponent } from './datatable/datatable.component';
+import { DataEditComponent } from './data-edit/data-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+const routes: Routes =[]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DataComponent,
+    DatatableComponent,
+    DataEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-edit/data-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-edit',
+  templateUrl: './data-edit.component.html',
+  styleUrls: ['./data-edit.component.css']
+})
+export class DataEditComponent {
+
+}
